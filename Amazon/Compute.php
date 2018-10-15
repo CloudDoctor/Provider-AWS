32964307ce53e5022aeaea85f0701fa580d4ba75
Matthew Baggett <matthew@baggett.me>
2018-10-15 22:45:45 +0100

    I can write AWS code, me.

--- Amazon/Compute.php
+++ Amazon/Compute.php
@@ -2,16 +2,98 @@
 
 namespace CloudDoctor\Amazon;
 
-use CloudDoctor\CloudDoctor;
+use Aws\Ec2\Ec2Client;
 use CloudDoctor\Common\ComputeGroup;
-use CloudDoctor\Exceptions\CloudDoctorException;
+use CloudDoctor\Common\Request;
 use CloudDoctor\Interfaces\ComputeInterface;
-use GuzzleHttp\Exception\ClientException;
-use phpseclib\Crypt\RSA;
+use CloudDoctor\Interfaces\RequestInterface;
 use phpseclib\Net\SFTP;
-use phpseclib\Net\SSH2;
 
-class Compute extends \CloudDoctor\Common\Compute implements ComputeInterface
+class Compute extends \CloudDoctor\Common\Compute
 {
-    
+    /** @var \CloudDoctor\Amazon\Request */
+    protected $requester;
+
+    public function __construct(ComputeGroup $computeGroup, $config = null)
+    {
+        parent::__construct($computeGroup, $config);
+    }
+
+    public function setRequester(RequestInterface $requester): ComputeInterface
+    {
+        parent::setRequester($requester);
+
+        foreach($this->config['region'] as $region) {
+            $this->requester->setupForRegion($region);
+        }
+
+        return $this;
+    }
+
+    public function deploy()
+    {
+        // TODO: Implement deploy() method.
+    }
+
+    public function exists(): bool
+    {
+        $allInstances = $this->requester->acrossRegionAction(
+            function(string $region, Ec2Client $ec2Client){
+                $instances = [];
+                foreach($ec2Client->describeInstances()->get('Reservations') as $instance){
+                    $id = $instance['Instances'][0]['InstanceId'];
+                    $regionField = ["Region" => $region];
+                    $instance['Instances'][0] = $regionField + $instance['Instances'][0];
+                    $instances[$id] = $instance['Instances'][0];
+                }
+                return $instances;
+            }
+        );
+
+        foreach($allInstances as $instance){
+            foreach($instance['Tags'] as $tag){
+                if($tag['Key'] == 'Name' && $tag['Value'] == $this->getName()){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function destroy(): bool
+    {
+        // TODO: Implement destroy() method.
+    }
+
+    public function getSshConnection(): ?SFTP
+    {
+        // TODO: Implement getSshConnection() method.
+    }
+
+    public function isTransitioning(): bool
+    {
+        // TODO: Implement isTransitioning() method.
+    }
+
+    public function isRunning(): bool
+    {
+        // TODO: Implement isRunning() method.
+    }
+
+    public function isStopped(): bool
+    {
+        // TODO: Implement isStopped() method.
+    }
+
+    public function getPublicIp(): string
+    {
+        // TODO: Implement getPublicIp() method.
+    }
+
+    public function updateMetaData(): void
+    {
+        // TODO: Implement updateMetaData() method.
+    }
+
 }
---
+++ Amazon/Request.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CloudDoctor\Amazon;
+
+use Aws\Ec2\Ec2Client;
+use GuzzleHttp\Client as GuzzleClient;
+use CloudDoctor\Interfaces\RequestInterface;
+
+class Request extends \CloudDoctor\Common\Request implements RequestInterface {
+
+    /** @var Ec2Client[] */
+    protected $ec2Clients = [];
+    protected $config;
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function setupForRegion(string $region) : Ec2Client
+    {
+        if(!isset($this->ec2Clients[$region])) {
+            $options = [
+                'region' => $region,
+                'version' => '2016-11-15',
+                'credentials' => [
+                    'key'    => $this->config['api-key'],
+                    'secret' => $this->config['api-secret'],
+                ],
+            ];
+            $this->ec2Clients[$region] = new Ec2Client($options);
+        }
+
+        return $this->ec2Clients[$region];
+    }
+
+    public function acrossRegionAction(callable $callable) : array
+    {
+        $merge = [];
+        foreach($this->ec2Clients as $region => $ec2Client){
+            $merge = array_merge(
+                $merge, call_user_func($callable, $region, $ec2Client) ?:[]
+            );
+        }
+        return $merge;
+    }
+}
